8237da81682f5b4f3e8681bfb3aef18086ef053d
emptyelephants <emptyelephants@gmail.com>
2018-02-06 08:18:24 -0800

    over to you dyl

--- drills.js
+++ drills.js
@@ -6,7 +6,7 @@ const knex = require('knex')(DATABASE);
 // clear the console before each run
 process.stdout.write('\x1Bc');
 
-// Sample select 
+// Sample select
 // knex
 //   .select()
 //   .from('restaurants')
@@ -26,12 +26,39 @@ process.stdout.write('\x1Bc');
 //   .limit(10)
 //   .then(results => console.log(results));
 
+//
+// knex
+//   .count()
+//   .from('restaurants')
+//   .where({ cuisine: 'Thai' })
+//   .then(results => console.log(results));
+//
+// knex
+//   .count()
+//   .from('restaurants')
+//   .then((results) => console.log(results));
+//-6
+// knex
+//   .count()
+//   .from('restaurants')
+//   .where({cuisine:'Thai',address_zipcode:'11372'})
+//   .then((results) => console.log(results));
+//
 
-knex
-  .count()
-  .from('restaurants')
-  .where({ cuisine: 'Thai' })
-  .then(results => console.log(results));
+// knex
+//   .select('name','id')
+//   .from('restaurants')
+//   .whereIn('address_zipcode',['10012','10013','10014'])
+//   .orderBy('name','asc')
+//   .limit(5)
+//   .then((result) => console.log(result))
+
+knex('restaurants')
+  .returning('name','id')
+  .insert({name: 'Byte Cafe', borough: 'Brooklyn',cuisine:'coffee',address_building_number: '123',address_street: 'Atlantic Avenue',address_zipcode: '11231'})
+  .then(console.log('done'));
+
+
 
 // Destroy the connection pool
 knex.destroy().then(() => {
